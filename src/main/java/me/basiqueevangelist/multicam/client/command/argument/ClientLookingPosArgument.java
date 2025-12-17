356126cc2db0bac050c1bfbd2ea475173466488c
justquacks <justquacksyt@gmail.com>
2025-12-17 04:35:39 -0600

    Update ClientLookingPosArgument.java

--- src/main/java/me/basiqueevangelist/multicam/client/command/argument/ClientLookingPosArgument.java
+++ src/main/java/me/basiqueevangelist/multicam/client/command/argument/ClientLookingPosArgument.java
@@ -2,49 +2,56 @@ package me.basiqueevangelist.multicam.client.command.argument;
 
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.command.argument.CoordinateArgument;
-import net.minecraft.command.argument.EntityAnchorArgumentType;
-import net.minecraft.command.argument.Vec3ArgumentType;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec2f;
-import net.minecraft.util.math.Vec3d;
-
-import java.util.Objects;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.commands.arguments.coordinates.WorldCoordinate;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 
 public class ClientLookingPosArgument implements ClientPosArgument {
-    private final double x;
-    private final double y;
-    private final double z;
+    private final WorldCoordinate left;
+    private final WorldCoordinate up;
+    private final WorldCoordinate forwards;
 
-    public ClientLookingPosArgument(double x, double y, double z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public ClientLookingPosArgument(WorldCoordinate left, WorldCoordinate up, WorldCoordinate forwards) {
+        this.left = left;
+        this.up = up;
+        this.forwards = forwards;
     }
 
     @Override
-    public Vec3d toAbsolutePos(FabricClientCommandSource source) {
-        Vec2f vec2f = source.getRotation();
-        Vec3d vec3d = EntityAnchorArgumentType.EntityAnchor.FEET.positionAt(source.getEntity());
-        float f = MathHelper.cos((vec2f.y + 90.0F) * (float) (Math.PI / 180.0));
-        float g = MathHelper.sin((vec2f.y + 90.0F) * (float) (Math.PI / 180.0));
-        float h = MathHelper.cos(-vec2f.x * (float) (Math.PI / 180.0));
-        float i = MathHelper.sin(-vec2f.x * (float) (Math.PI / 180.0));
-        float j = MathHelper.cos((-vec2f.x + 90.0F) * (float) (Math.PI / 180.0));
-        float k = MathHelper.sin((-vec2f.x + 90.0F) * (float) (Math.PI / 180.0));
-        Vec3d vec3d2 = new Vec3d(f * h, i, g * h);
-        Vec3d vec3d3 = new Vec3d(f * j, k, g * j);
-        Vec3d vec3d4 = vec3d2.crossProduct(vec3d3).multiply(-1.0);
-        double d = vec3d2.x * this.z + vec3d3.x * this.y + vec3d4.x * this.x;
-        double e = vec3d2.y * this.z + vec3d3.y * this.y + vec3d4.y * this.x;
-        double l = vec3d2.z * this.z + vec3d3.z * this.y + vec3d4.z * this.x;
-        return new Vec3d(vec3d.x + d, vec3d.y + e, vec3d.z + l);
+    public Vec3 toAbsolutePos(LocalPlayer player, ClientLevel level) {
+        Vec3 playerPos = player.position();
+        float yaw = player.getYRot();
+        float pitch = player.getXRot();
+        
+        float yawRad = yaw * (float) (Math.PI / 180.0);
+        float pitchRad = pitch * (float) (Math.PI / 180.0);
+        
+        float cosYaw = (float) Math.cos(yawRad);
+        float sinYaw = (float) Math.sin(yawRad);
+        float cosPitch = (float) Math.cos(pitchRad);
+        float sinPitch = (float) Math.sin(pitchRad);
+        
+        // Extract coordinate values - for local coordinates, we use 0 as the base
+        double leftVal = left.get(0);
+        double upVal = up.get(0);
+        double forwardsVal = forwards.get(0);
+        
+        Vec3 leftVec = new Vec3(-sinYaw, 0, cosYaw);
+        Vec3 upVec = new Vec3(0, 1, 0);
+        Vec3 forwardVec = new Vec3(cosYaw * cosPitch, -sinPitch, sinYaw * cosPitch);
+        
+        Vec3 offset = leftVec.scale(leftVal)
+            .add(upVec.scale(upVal))
+            .add(forwardVec.scale(-forwardsVal));
+        
+        return playerPos.add(offset);
     }
 
     @Override
-    public Vec2f toAbsoluteRotation(FabricClientCommandSource source) {
-        return Vec2f.ZERO;
+    public Vec2 toAbsoluteRotation(LocalPlayer player) {
+        return player.getRotationVector();
     }
 
     @Override
@@ -63,48 +70,25 @@ public class ClientLookingPosArgument implements ClientPosArgument {
     }
 
     public static ClientLookingPosArgument parse(StringReader reader) throws CommandSyntaxException {
-        int i = reader.getCursor();
-        double d = readCoordinate(reader, i);
-        if (reader.canRead() && reader.peek() == ' ') {
-            reader.skip();
-            double e = readCoordinate(reader, i);
-            if (reader.canRead() && reader.peek() == ' ') {
-                reader.skip();
-                double f = readCoordinate(reader, i);
-                return new ClientLookingPosArgument(d, e, f);
-            } else {
-                reader.setCursor(i);
-                throw Vec3ArgumentType.INCOMPLETE_EXCEPTION.createWithContext(reader);
-            }
-        } else {
-            reader.setCursor(i);
-            throw Vec3ArgumentType.INCOMPLETE_EXCEPTION.createWithContext(reader);
+        int cursor = reader.getCursor();
+        WorldCoordinate left = WorldCoordinate.parseDouble(reader, false);
+        
+        if (!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(cursor);
+            throw CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherParseException().createWithContext(reader, "Expected 3 coordinates");
         }
-    }
-
-    private static double readCoordinate(StringReader reader, int startingCursorPos) throws CommandSyntaxException {
-        if (!reader.canRead()) {
-            throw CoordinateArgument.MISSING_COORDINATE.createWithContext(reader);
-        } else if (reader.peek() != '^') {
-            reader.setCursor(startingCursorPos);
-            throw Vec3ArgumentType.MIXED_COORDINATE_EXCEPTION.createWithContext(reader);
-        } else {
-            reader.skip();
-            return reader.canRead() && reader.peek() != ' ' ? reader.readDouble() : 0.0;
+        
+        reader.skip();
+        WorldCoordinate up = WorldCoordinate.parseDouble(reader, false);
+        
+        if (!reader.canRead() || reader.peek() != ' ') {
+            reader.setCursor(cursor);
+            throw CommandSyntaxException.BUILT_IN_EXCEPTIONS.dispatcherParseException().createWithContext(reader, "Expected 3 coordinates");
         }
-    }
-
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        } else {
-            return !(o instanceof ClientLookingPosArgument lookingPosArgument)
-                ? false
-                : this.x == lookingPosArgument.x && this.y == lookingPosArgument.y && this.z == lookingPosArgument.z;
-        }
-    }
-
-    public int hashCode() {
-        return Objects.hash(this.x, this.y, this.z);
+        
+        reader.skip();
+        WorldCoordinate forwards = WorldCoordinate.parseDouble(reader, false);
+        
+        return new ClientLookingPosArgument(left, up, forwards);
     }
 }
