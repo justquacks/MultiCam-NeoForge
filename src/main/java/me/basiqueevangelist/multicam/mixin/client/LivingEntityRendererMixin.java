e208bc876ce670b50bcebe46da70e4a206f5deea
justquacks <justquacksyt@gmail.com>
2025-12-17 04:26:54 -0600

    Update LivingEntityRendererMixin.java

--- src/main/java/me/basiqueevangelist/multicam/mixin/client/LivingEntityRendererMixin.java
+++ src/main/java/me/basiqueevangelist/multicam/mixin/client/LivingEntityRendererMixin.java
@@ -1,9 +1,9 @@
 package me.basiqueevangelist.multicam.mixin.client;
 
 import me.basiqueevangelist.multicam.client.WorldViewComponent;
-import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.render.entity.LivingEntityRenderer;
-import net.minecraft.entity.LivingEntity;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.client.renderer.entity.LivingEntityRenderer;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,9 +11,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntityRenderer.class)
 public class LivingEntityRendererMixin {
-    @Inject(method = "hasLabel(Lnet/minecraft/entity/LivingEntity;)Z", at = @At("HEAD"), cancellable = true)
+    @Inject(method = "shouldShowName(Lnet/minecraft/world/entity/LivingEntity;)Z", at = @At("HEAD"), cancellable = true)
     private void makeNameRender(LivingEntity livingEntity, CallbackInfoReturnable<Boolean> cir) {
-        if (livingEntity instanceof ClientPlayerEntity && WorldViewComponent.CURRENT_BUFFER != null)
+        if (livingEntity instanceof LocalPlayer && WorldViewComponent.CURRENT_BUFFER != null)
             cir.setReturnValue(true);
     }
 }
