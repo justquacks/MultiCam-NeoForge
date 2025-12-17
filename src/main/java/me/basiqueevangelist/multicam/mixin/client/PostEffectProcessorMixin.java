9729596751ba4401fb68c0621e724323643982e6
justquacks <justquacksyt@gmail.com>
2025-12-17 04:26:25 -0600

    Update PostEffectProcessorMixin.java

--- src/main/java/me/basiqueevangelist/multicam/mixin/client/PostEffectProcessorMixin.java
+++ src/main/java/me/basiqueevangelist/multicam/mixin/client/PostEffectProcessorMixin.java
@@ -1,24 +1,24 @@
 package me.basiqueevangelist.multicam.mixin.client;
 
 import me.basiqueevangelist.multicam.client.DelegatingFramebuffer;
-import net.minecraft.client.gl.Framebuffer;
-import net.minecraft.client.gl.PostEffectProcessor;
+import com.mojang.blaze3d.pipeline.RenderTarget;
+import net.minecraft.client.renderer.PostChain;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
-@Mixin(PostEffectProcessor.class)
+@Mixin(PostChain.class)
 public class PostEffectProcessorMixin {
     @ModifyVariable(method = "<init>", at = @At("HEAD"), argsOnly = true)
-    private static Framebuffer trackifyFramebuffer(Framebuffer framebuffer) {
+    private static RenderTarget trackifyFramebuffer(RenderTarget framebuffer) {
         if (framebuffer instanceof DelegatingFramebuffer)
             return framebuffer;
         else
             return new DelegatingFramebuffer(framebuffer);
     }
 
-    @ModifyVariable(method = "addTarget", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gl/Framebuffer;setClearColor(FFFF)V"))
-    private Framebuffer trackify(Framebuffer old) {
+    @ModifyVariable(method = "addTempTarget", at = @At(value = "INVOKE", target = "Lcom/mojang/blaze3d/pipeline/RenderTarget;setClearColor(FFFF)V"))
+    private RenderTarget trackify(RenderTarget old) {
         return new DelegatingFramebuffer(old);
     }
 }
