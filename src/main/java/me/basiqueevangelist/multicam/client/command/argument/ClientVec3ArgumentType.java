246f64cb74e79e2b38788b8d57119f7d8fc199c6
justquacks <justquacksyt@gmail.com>
2025-12-17 04:34:01 -0600

    Update ClientVec3ArgumentType.java

--- src/main/java/me/basiqueevangelist/multicam/client/command/argument/ClientVec3ArgumentType.java
+++ src/main/java/me/basiqueevangelist/multicam/client/command/argument/ClientVec3ArgumentType.java
@@ -6,10 +6,14 @@ import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.command.CommandSource;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.commands.Commands;
+import net.minecraft.commands.SharedSuggestionProvider;
+import net.minecraft.commands.arguments.coordinates.Coordinates;
+import net.minecraft.commands.arguments.coordinates.LocalCoordinates;
+import net.minecraft.world.phys.Vec3;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -17,7 +21,10 @@ import java.util.Collections;
 import java.util.concurrent.CompletableFuture;
 
 public class ClientVec3ArgumentType implements ArgumentType<ClientPosArgument> {
-    private static final Collection<String> EXAMPLES = Arrays.asList("0 0 0", "~ ~ ~", "^ ^ ^", "^1 ^ ^-5", "0.1 -0.5 .9", "~0.5 ~1 ~-5");
+    private static final Collection<String> EXAMPLES = Arrays.asList(
+        "0 0 0", "~ ~ ~", "^ ^ ^", "^1 ^ ^-5", "0.1 -0.5 .9", "~0.5 ~1 ~-5"
+    );
+
     private final boolean centerIntegers;
 
     public ClientVec3ArgumentType(boolean centerIntegers) {
@@ -32,35 +39,38 @@ public class ClientVec3ArgumentType implements ArgumentType<ClientPosArgument> {
         return new ClientVec3ArgumentType(centerIntegers);
     }
 
-    public static Vec3d getVec3(CommandContext<FabricClientCommandSource> context, String name) {
-        return context.getArgument(name, ClientPosArgument.class).toAbsolutePos(context.getSource());
+    public static Vec3 getVec3(CommandContext<?> context, String name, LocalPlayer player, ClientLevel level) {
+        return context.getArgument(name, ClientPosArgument.class)
+            .toAbsolutePos(player, level);
     }
 
-    public static ClientPosArgument getPosArgument(CommandContext<FabricClientCommandSource> context, String name) {
+    public static ClientPosArgument getPosArgument(CommandContext<?> context, String name) {
         return context.getArgument(name, ClientPosArgument.class);
     }
 
-    public ClientPosArgument parse(StringReader stringReader) throws CommandSyntaxException {
-        return stringReader.canRead() && stringReader.peek() == '^'
-            ? ClientLookingPosArgument.parse(stringReader)
-            : ClientDefaultPosArgument.parse(stringReader, this.centerIntegers);
+    @Override
+    public ClientPosArgument parse(StringReader reader) throws CommandSyntaxException {
+        return reader.canRead() && reader.peek() == '^'
+            ? ClientLookingPosArgument.parse(reader)
+            : ClientDefaultPosArgument.parse(reader, this.centerIntegers);
     }
 
     @Override
     public <S> CompletableFuture<Suggestions> listSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
-        if (!(context.getSource() instanceof CommandSource)) {
+        if (!(context.getSource() instanceof SharedSuggestionProvider)) {
             return Suggestions.empty();
-        } else {
-            String string = builder.getRemaining();
-            Collection<CommandSource.RelativePosition> collection;
-            if (!string.isEmpty() && string.charAt(0) == '^') {
-                collection = Collections.singleton(CommandSource.RelativePosition.ZERO_LOCAL);
-            } else {
-                collection = ((CommandSource)context.getSource()).getPositionSuggestions();
-            }
-
-            return CommandSource.suggestPositions(string, collection, builder, CommandManager.getCommandValidator(this::parse));
         }
+
+        String remaining = builder.getRemaining();
+        Collection<SharedSuggestionProvider.TextCoordinates> coords;
+
+        if (!remaining.isEmpty() && remaining.charAt(0) == '^') {
+            coords = Collections.singleton(SharedSuggestionProvider.TextCoordinates.DEFAULT_LOCAL);
+        } else {
+            coords = Collections.singleton(SharedSuggestionProvider.TextCoordinates.DEFAULT_GLOBAL);
+        }
+
+        return SharedSuggestionProvider.suggestCoordinates(remaining, coords, builder, Commands.createValidator(this::parse));
     }
 
     @Override
@@ -68,4 +78,3 @@ public class ClientVec3ArgumentType implements ArgumentType<ClientPosArgument> {
         return EXAMPLES;
     }
 }
-
