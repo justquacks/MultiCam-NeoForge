52bf091e2392ff7114b6f88d04a1e9dbc74d08e7
justquacks <justquacksyt@gmail.com>
2025-12-17 04:27:15 -0600

    Update BlockEntityRenderDispatcherMixin.java

--- src/main/java/me/basiqueevangelist/multicam/mixin/client/BlockEntityRenderDispatcherMixin.java
+++ src/main/java/me/basiqueevangelist/multicam/mixin/client/BlockEntityRenderDispatcherMixin.java
@@ -1,10 +1,10 @@
 package me.basiqueevangelist.multicam.mixin.client;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import me.basiqueevangelist.multicam.client.WorldViewComponent;
-import net.minecraft.block.entity.BlockEntity;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.blockentity.BlockEntityRenderDispatcher;
+import net.minecraft.world.level.block.entity.BlockEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BlockEntityRenderDispatcher.class)
 public class BlockEntityRenderDispatcherMixin {
-    @Inject(method = "render(Lnet/minecraft/block/entity/BlockEntity;FLnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;)V", at = @At("HEAD"), cancellable = true)
-    private void disableBlockEntitiesIfNeeded(BlockEntity blockEntity, float tickDelta, MatrixStack matrices, VertexConsumerProvider vertexConsumers, CallbackInfo ci) {
+    @Inject(method = "render(Lnet/minecraft/world/level/block/entity/BlockEntity;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;)V", at = @At("HEAD"), cancellable = true)
+    private void disableBlockEntitiesIfNeeded(BlockEntity blockEntity, float tickDelta, PoseStack matrices, MultiBufferSource vertexConsumers, CallbackInfo ci) {
         if (WorldViewComponent.CURRENT != null && WorldViewComponent.CURRENT.disableBlockEntities())
             ci.cancel();
     }
