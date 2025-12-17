9dff42535ad9cbde14047b69f9f7484dfa417652
justquacks <justquacksyt@gmail.com>
2025-12-17 04:33:11 -0600

    Update ConfigCameraCommand.java

--- src/main/java/me/basiqueevangelist/multicam/client/command/ConfigCameraCommand.java
+++ src/main/java/me/basiqueevangelist/multicam/client/command/ConfigCameraCommand.java
@@ -3,13 +3,13 @@ package me.basiqueevangelist.multicam.client.command;
 import com.mojang.brigadier.arguments.BoolArgumentType;
 import com.mojang.brigadier.builder.ArgumentBuilder;
 import me.basiqueevangelist.multicam.client.CameraWindow;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
+import net.minecraft.commands.CommandSourceStack;
 
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+import static net.minecraft.commands.Commands.argument;
+import static net.minecraft.commands.Commands.literal;
 
 public class ConfigCameraCommand {
-    public static ArgumentBuilder<FabricClientCommandSource, ?> build() {
+    public static ArgumentBuilder<CommandSourceStack, ?> build() {
         return literal("config")
             .then(CommandUtil.cameraNode()
                 .then(literal("entities")
