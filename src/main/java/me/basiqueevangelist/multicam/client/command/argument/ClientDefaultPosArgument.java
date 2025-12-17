415a147f547bbb9c078d0599aee159ceb639a5c8
justquacks <justquacksyt@gmail.com>
2025-12-17 04:35:50 -0600

    Update ClientDefaultPosArgument.java

--- src/main/java/me/basiqueevangelist/multicam/client/command/argument/ClientDefaultPosArgument.java
+++ src/main/java/me/basiqueevangelist/multicam/client/command/argument/ClientDefaultPosArgument.java
@@ -2,117 +2,52 @@ package me.basiqueevangelist.multicam.client.command.argument;
 
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.command.argument.CoordinateArgument;
-import net.minecraft.command.argument.Vec3ArgumentType;
-import net.minecraft.util.math.Vec2f;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.multiplayer.ClientLevel;
+import net.minecraft.client.player.LocalPlayer;
+import net.minecraft.commands.arguments.coordinates.WorldCoordinates;
+import net.minecraft.world.phys.Vec2;
+import net.minecraft.world.phys.Vec3;
 
 public class ClientDefaultPosArgument implements ClientPosArgument {
-    private final CoordinateArgument x;
-    private final CoordinateArgument y;
-    private final CoordinateArgument z;
+    private final WorldCoordinates worldCoordinates;
 
-    public ClientDefaultPosArgument(CoordinateArgument x, CoordinateArgument y, CoordinateArgument z) {
-        this.x = x;
-        this.y = y;
-        this.z = z;
+    public ClientDefaultPosArgument(WorldCoordinates worldCoordinates) {
+        this.worldCoordinates = worldCoordinates;
     }
 
     @Override
-    public Vec3d toAbsolutePos(FabricClientCommandSource source) {
-        Vec3d vec3d = source.getPosition();
-        return new Vec3d(this.x.toAbsoluteCoordinate(vec3d.x), this.y.toAbsoluteCoordinate(vec3d.y), this.z.toAbsoluteCoordinate(vec3d.z));
+    public Vec3 toAbsolutePos(LocalPlayer player, ClientLevel level) {
+        // Use the player's command source stack to resolve coordinates
+        return worldCoordinates.getPosition(player.createCommandSourceStack());
     }
 
     @Override
-    public Vec2f toAbsoluteRotation(FabricClientCommandSource source) {
-        Vec2f vec2f = source.getRotation();
-        return new Vec2f((float)this.x.toAbsoluteCoordinate(vec2f.x), (float)this.y.toAbsoluteCoordinate(vec2f.y));
+    public Vec2 toAbsoluteRotation(LocalPlayer player) {
+        // Use the player's command source stack to resolve rotation
+        return worldCoordinates.getRotation(player.createCommandSourceStack());
     }
 
     @Override
     public boolean isXRelative() {
-        return this.x.isRelative();
+        return worldCoordinates.isXRelative();
     }
 
     @Override
     public boolean isYRelative() {
-        return this.y.isRelative();
+        return worldCoordinates.isYRelative();
     }
 
     @Override
     public boolean isZRelative() {
-        return this.z.isRelative();
-    }
-
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        } else if (!(o instanceof ClientDefaultPosArgument defaultPosArgument)) {
-            return false;
-        } else if (!this.x.equals(defaultPosArgument.x)) {
-            return false;
-        } else {
-            return !this.y.equals(defaultPosArgument.y) ? false : this.z.equals(defaultPosArgument.z);
-        }
-    }
-
-    public static ClientDefaultPosArgument parse(StringReader reader) throws CommandSyntaxException {
-        int i = reader.getCursor();
-        CoordinateArgument coordinateArgument = CoordinateArgument.parse(reader);
-        if (reader.canRead() && reader.peek() == ' ') {
-            reader.skip();
-            CoordinateArgument coordinateArgument2 = CoordinateArgument.parse(reader);
-            if (reader.canRead() && reader.peek() == ' ') {
-                reader.skip();
-                CoordinateArgument coordinateArgument3 = CoordinateArgument.parse(reader);
-                return new ClientDefaultPosArgument(coordinateArgument, coordinateArgument2, coordinateArgument3);
-            } else {
-                reader.setCursor(i);
-                throw Vec3ArgumentType.INCOMPLETE_EXCEPTION.createWithContext(reader);
-            }
-        } else {
-            reader.setCursor(i);
-            throw Vec3ArgumentType.INCOMPLETE_EXCEPTION.createWithContext(reader);
-        }
+        return worldCoordinates.isZRelative();
     }
 
     public static ClientDefaultPosArgument parse(StringReader reader, boolean centerIntegers) throws CommandSyntaxException {
-        int i = reader.getCursor();
-        CoordinateArgument coordinateArgument = CoordinateArgument.parse(reader, centerIntegers);
-        if (reader.canRead() && reader.peek() == ' ') {
-            reader.skip();
-            CoordinateArgument coordinateArgument2 = CoordinateArgument.parse(reader, false);
-            if (reader.canRead() && reader.peek() == ' ') {
-                reader.skip();
-                CoordinateArgument coordinateArgument3 = CoordinateArgument.parse(reader, centerIntegers);
-                return new ClientDefaultPosArgument(coordinateArgument, coordinateArgument2, coordinateArgument3);
-            } else {
-                reader.setCursor(i);
-                throw Vec3ArgumentType.INCOMPLETE_EXCEPTION.createWithContext(reader);
-            }
-        } else {
-            reader.setCursor(i);
-            throw Vec3ArgumentType.INCOMPLETE_EXCEPTION.createWithContext(reader);
-        }
-    }
-
-    public static ClientDefaultPosArgument absolute(double x, double y, double z) {
-        return new ClientDefaultPosArgument(new CoordinateArgument(false, x), new CoordinateArgument(false, y), new CoordinateArgument(false, z));
-    }
-
-    public static ClientDefaultPosArgument absolute(Vec2f vec) {
-        return new ClientDefaultPosArgument(new CoordinateArgument(false, vec.x), new CoordinateArgument(false, vec.y), new CoordinateArgument(true, 0.0));
-    }
-
-    public static ClientDefaultPosArgument zero() {
-        return new ClientDefaultPosArgument(new CoordinateArgument(true, 0.0), new CoordinateArgument(true, 0.0), new CoordinateArgument(true, 0.0));
-    }
-
-    public int hashCode() {
-        int i = this.x.hashCode();
-        i = 31 * i + this.y.hashCode();
-        return 31 * i + this.z.hashCode();
+        int cursor = reader.getCursor();
+        
+        // WorldCoordinates.parseInt() no longer takes a boolean parameter in 1.21.1
+        WorldCoordinates coords = WorldCoordinates.parseInt(reader);
+        
+        return new ClientDefaultPosArgument(coords);
     }
 }
