09585257d9aa7a413a73652dedf86bf8a345f16a
justquacks <justquacksyt@gmail.com>
2025-12-17 04:30:32 -0600

    Update AnimatableFloat.java

--- src/main/java/me/basiqueevangelist/multicam/client/AnimatableFloat.java
+++ src/main/java/me/basiqueevangelist/multicam/client/AnimatableFloat.java
@@ -1,11 +1,11 @@
 package me.basiqueevangelist.multicam.client;
 
 import me.basiqueevangelist.multicam.client.owocode.Animatable;
-import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.Mth;
 
 public record AnimatableFloat(float inner) implements Animatable<AnimatableFloat> {
     @Override
     public AnimatableFloat interpolate(AnimatableFloat next, float delta) {
-        return new AnimatableFloat(MathHelper.lerp(delta, this.inner, next.inner));
+        return new AnimatableFloat(Mth.lerp(delta, this.inner, next.inner));
     }
 }
