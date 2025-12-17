413b3bb274741fb8af4e452f25138b9824031193
justquacks <justquacksyt@gmail.com>
2025-12-17 04:29:21 -0600

    Update MultiCam.java

--- src/main/java/me/basiqueevangelist/multicam/client/MultiCam.java
+++ src/main/java/me/basiqueevangelist/multicam/client/MultiCam.java
@@ -6,63 +6,80 @@ import me.basiqueevangelist.multicam.client.command.MoveCameraCommand;
 import me.basiqueevangelist.multicam.client.command.OrbitCameraCommand;
 import me.basiqueevangelist.multicam.client.command.ZoomCameraCommand;
 import me.basiqueevangelist.windowapi.OpenWindows;
-import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.ShaderInstance;
+import net.minecraft.resources.ResourceLocation;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.neoforge.client.event.ClientPlayerNetworkEvent;
+import net.neoforged.neoforge.client.event.RegisterClientCommandsEvent;
+import net.neoforged.neoforge.client.event.RegisterShadersEvent;
+import com.mojang.blaze3d.vertex.DefaultVertexFormat;
 
-import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.fabricmc.fabric.api.client.rendering.v1.CoreShaderRegistrationCallback;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.ShaderProgram;
-import net.minecraft.client.render.VertexFormats;
-import net.minecraft.util.Identifier;
+import static net.minecraft.commands.Commands.argument;
+import static net.minecraft.commands.Commands.literal;
 
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.argument;
-import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
+@EventBusSubscriber(modid = "multicam", value = Dist.CLIENT)
+public class MultiCam {
+    public static ShaderInstance WORLD_VIEW_PROGRAM = null;
 
-public class MultiCam implements ClientModInitializer {
-	public static ShaderProgram WORLD_VIEW_PROGRAM = null;
+    public static int FPS_TARGET = 60;
 
-	public static int FPS_TARGET = 60;
+    @SubscribeEvent
+    public static void onClientSetup(FMLClientSetupEvent event) {
+        // Empty again idk 
+    }
 
-	@Override
-	public void onInitializeClient() {
-		ServerData.init();
+    @SubscribeEvent
+    public static void onRegisterShaders(RegisterShadersEvent event) {
+        try {
+            event.registerShader(
+                new ShaderInstance(
+                    event.getResourceProvider(),
+                    ResourceLocation.fromNamespaceAndPath("multicam", "world_view"),
+                    DefaultVertexFormat.POSITION_TEX
+                ),
+                shader -> WORLD_VIEW_PROGRAM = shader
+            );
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to register shader", e);
+        }
+    }
 
-		CoreShaderRegistrationCallback.EVENT.register(context -> {
-			context.register(Identifier.of("multicam", "world_view"), VertexFormats.POSITION_COLOR, program -> WORLD_VIEW_PROGRAM = program);
-		});
+    @SubscribeEvent
+    public static void onRegisterCommands(RegisterClientCommandsEvent event) {
+        event.getDispatcher().register(
+            literal("multicam")
+                .requires(x -> ServerData.canUse(x.hasPermission(2)))
+                .executes(context -> {
+                    new CameraWindow().open();
+                    return 1;
+                })
+                .then(literal("fps_target")
+                    .then(argument("target", IntegerArgumentType.integer(1))
+                        .executes(ctx -> {
+                            FPS_TARGET = IntegerArgumentType.getInteger(ctx, "target");
+                            return 0;
+                        })))
+                .then(MoveCameraCommand.build())
+                .then(ZoomCameraCommand.build())
+                .then(OrbitCameraCommand.build())
+                .then(ConfigCameraCommand.build())
+        );
+    }
 
-		ClientCommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> {
-			dispatcher.register(
-				literal("multicam")
-					.requires(x -> ServerData.canUse(x.hasPermissionLevel(2)))
-					.executes(context -> {
-						new CameraWindow().open();
-						return 1;
-					})
-					.then(literal("fps_target")
-						.then(argument("target", IntegerArgumentType.integer(1))
-							.executes(ctx -> {
-								FPS_TARGET = IntegerArgumentType.getInteger(ctx, "target");
-								return 0;
-							})))
-					.then(MoveCameraCommand.build())
-					.then(ZoomCameraCommand.build())
-					.then(OrbitCameraCommand.build())
-					.then(ConfigCameraCommand.build())
-			);
-		});
+    @SubscribeEvent
+    public static void onClientDisconnect(ClientPlayerNetworkEvent.LoggingOut event) {
+        closeAllWindows();
+    }
 
-		ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
-			closeAllWindows();
-		});
-	}
-
-	public static void closeAllWindows() {
-		OpenWindows.windows().forEach(x -> {
-			if (x instanceof CameraWindow) {
-				MinecraftClient.getInstance().send(x::close);
-			}
-		});
-	}
+    public static void closeAllWindows() {
+        OpenWindows.windows().forEach(x -> {
+            if (x instanceof CameraWindow) {
+                Minecraft.getInstance().execute(x::close);
+            }
+        });
+    }
 }
