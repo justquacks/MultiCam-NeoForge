2f2c02251f6dda60d8c2b4c6765efe8ab86a679d
justquacks <justquacksyt@gmail.com>
2025-12-17 04:24:00 -0600

    Update WindowContext.java

--- src/main/java/me/basiqueevangelist/windowapi/context/WindowContext.java
+++ src/main/java/me/basiqueevangelist/windowapi/context/WindowContext.java
@@ -1,15 +1,17 @@
 package me.basiqueevangelist.windowapi.context;
 
+import com.mojang.blaze3d.pipeline.RenderTarget;
 import me.basiqueevangelist.windowapi.SupportsFeatures;
-import me.basiqueevangelist.windowapi.WindowFramebufferResized;
-import net.fabricmc.fabric.api.event.Event;
-import net.minecraft.client.gl.Framebuffer;
+
+import java.util.function.Consumer;
 
 public interface WindowContext extends SupportsFeatures<WindowContext> {
     int framebufferWidth();
     int framebufferHeight();
-    Event<WindowFramebufferResized> framebufferResized();
-    Framebuffer framebuffer();
+    
+    void onFramebufferResized(Consumer<WindowContext> callback);
+    
+    RenderTarget framebuffer();
 
     int scaledWidth();
     int scaledHeight();
