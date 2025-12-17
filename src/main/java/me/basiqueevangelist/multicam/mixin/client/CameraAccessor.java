cf673ee2e383932a6d63d2bf30622e143aaff914
justquacks <justquacksyt@gmail.com>
2025-12-17 04:27:09 -0600

    Update CameraAccessor.java

--- src/main/java/me/basiqueevangelist/multicam/mixin/client/CameraAccessor.java
+++ src/main/java/me/basiqueevangelist/multicam/mixin/client/CameraAccessor.java
@@ -1,15 +1,15 @@
 package me.basiqueevangelist.multicam.mixin.client;
 
-import net.minecraft.client.render.Camera;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.client.Camera;
+import net.minecraft.world.phys.Vec3;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Invoker;
 
 @Mixin(Camera.class)
 public interface CameraAccessor {
-    @Invoker
-    void invokeSetPos(Vec3d pos);
+    @Invoker("setPosition")
+    void invokeSetPosition(Vec3 pos);
 
-    @Invoker
+    @Invoker("setRotation")
     void invokeSetRotation(float yaw, float pitch);
 }
