555c27500b113b7344f6a648832be979970e874e
justquacks <justquacksyt@gmail.com>
2025-12-17 04:27:02 -0600

    Update KeyBindingAccessor.java

--- src/main/java/me/basiqueevangelist/multicam/mixin/client/KeyBindingAccessor.java
+++ src/main/java/me/basiqueevangelist/multicam/mixin/client/KeyBindingAccessor.java
@@ -1,12 +1,12 @@
 package me.basiqueevangelist.multicam.mixin.client;
 
-import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
+import com.mojang.blaze3d.platform.InputConstants;
+import net.minecraft.client.KeyMapping;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
-@Mixin(KeyBinding.class)
+@Mixin(KeyMapping.class)
 public interface KeyBindingAccessor {
-    @Accessor
-    InputUtil.Key getBoundKey();
+    @Accessor("key")
+    InputConstants.Key getBoundKey();
 }
