b6e8bc89a51edb2d29a2e704fac4816c8ca2b4ad
justquacks <justquacksyt@gmail.com>
2025-12-17 04:30:19 -0600

    Update AnimatableVec3d.java

--- src/main/java/me/basiqueevangelist/multicam/client/AnimatableVec3d.java
+++ src/main/java/me/basiqueevangelist/multicam/client/AnimatableVec3d.java
@@ -1,17 +1,17 @@
 package me.basiqueevangelist.multicam.client;
 
 import me.basiqueevangelist.multicam.client.owocode.Animatable;
-import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.Mth;
+import net.minecraft.world.phys.Vec3;
 
-public record AnimatableVec3d(Vec3d inner) implements Animatable<AnimatableVec3d> {
+public record AnimatableVec3d(Vec3 inner) implements Animatable<AnimatableVec3d> {
     @Override
     public AnimatableVec3d interpolate(AnimatableVec3d next, float delta) {
         return new AnimatableVec3d(
-            new Vec3d(
-                MathHelper.lerp(delta, this.inner.x, next.inner.x),
-                MathHelper.lerp(delta, this.inner.y, next.inner.y),
-                MathHelper.lerp(delta, this.inner.z, next.inner.z)
+            new Vec3(
+                Mth.lerp(delta, this.inner.x, next.inner.x),
+                Mth.lerp(delta, this.inner.y, next.inner.y),
+                Mth.lerp(delta, this.inner.z, next.inner.z)
             )
         );
     }
