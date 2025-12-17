e19c799acdc9ed25e2e79d00dfddb53f4786d0f4
justquacks <justquacksyt@gmail.com>
2025-12-17 04:34:50 -0600

    Update ClientRotationArgumentType.java

--- src/main/java/me/basiqueevangelist/multicam/client/command/argument/ClientRotationArgumentType.java
+++ src/main/java/me/basiqueevangelist/multicam/client/command/argument/ClientRotationArgumentType.java
@@ -5,9 +5,9 @@ import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
-import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
-import net.minecraft.command.argument.CoordinateArgument;
-import net.minecraft.text.Text;
+import net.minecraft.commands.arguments.coordinates.WorldCoordinate;
+import net.minecraft.commands.arguments.coordinates.WorldCoordinates;
+import net.minecraft.network.chat.Component;
 
 import java.util.Arrays;
 import java.util.Collection;
@@ -15,27 +15,32 @@ import java.util.Collection;
 public class ClientRotationArgumentType implements ArgumentType<ClientPosArgument> {
     private static final Collection<String> EXAMPLES = Arrays.asList("0 0", "~ ~", "~-5 ~5");
     public static final SimpleCommandExceptionType INCOMPLETE_ROTATION_EXCEPTION = new SimpleCommandExceptionType(
-        Text.translatable("argument.rotation.incomplete")
+        Component.translatable("argument.rotation.incomplete")
     );
 
     public static ClientRotationArgumentType rotation() {
         return new ClientRotationArgumentType();
     }
 
-    public static ClientPosArgument getRotation(CommandContext<FabricClientCommandSource> context, String name) {
+    public static ClientPosArgument getRotation(CommandContext<?> context, String name) {
         return context.getArgument(name, ClientPosArgument.class);
     }
 
+    @Override
     public ClientPosArgument parse(StringReader stringReader) throws CommandSyntaxException {
         int i = stringReader.getCursor();
         if (!stringReader.canRead()) {
             throw INCOMPLETE_ROTATION_EXCEPTION.createWithContext(stringReader);
         } else {
-            CoordinateArgument coordinateArgument = CoordinateArgument.parse(stringReader, false);
+            WorldCoordinate coordinateArgument = WorldCoordinate.parseDouble(stringReader, false);
             if (stringReader.canRead() && stringReader.peek() == ' ') {
                 stringReader.skip();
-                CoordinateArgument coordinateArgument2 = CoordinateArgument.parse(stringReader, false);
-                return new ClientDefaultPosArgument(coordinateArgument2, coordinateArgument, new CoordinateArgument(true, 0.0));
+                WorldCoordinate coordinateArgument2 = WorldCoordinate.parseDouble(stringReader, false);
+                // Create a zero coordinate for Z axis in rotation
+                WorldCoordinate zeroCoord = new WorldCoordinate(true, 0);
+                // Create a WorldCoordinates object for rotation (pitch, yaw, 0)
+                WorldCoordinates coords = new WorldCoordinates(coordinateArgument2, coordinateArgument, zeroCoord);
+                return new ClientDefaultPosArgument(coords);
             } else {
                 stringReader.setCursor(i);
                 throw INCOMPLETE_ROTATION_EXCEPTION.createWithContext(stringReader);
@@ -48,4 +53,3 @@ public class ClientRotationArgumentType implements ArgumentType<ClientPosArgumen
         return EXAMPLES;
     }
 }
-
