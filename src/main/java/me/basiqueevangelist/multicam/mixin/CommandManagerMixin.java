bb679a14faba74f3591e9dd35b7ab66c73ce42e4
justquacks <justquacksyt@gmail.com>
2025-12-17 04:25:13 -0600

    Update CommandManagerMixin.java

--- src/main/java/me/basiqueevangelist/multicam/mixin/CommandManagerMixin.java
+++ src/main/java/me/basiqueevangelist/multicam/mixin/CommandManagerMixin.java
@@ -1,17 +1,18 @@
 package me.basiqueevangelist.multicam.mixin;
 
 import me.basiqueevangelist.multicam.common.MultiCamCommon;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.commands.CommandBuildContext;
+import net.minecraft.commands.Commands;
+import net.minecraft.server.level.ServerPlayer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(CommandManager.class)
+@Mixin(Commands.class)
 public class CommandManagerMixin {
-    @Inject(method = "sendCommandTree(Lnet/minecraft/server/network/ServerPlayerEntity;)V", at = @At("HEAD"))
-    private void onReloadPermissions(ServerPlayerEntity player, CallbackInfo ci) {
+    @Inject(method = "sendCommands", at = @At("HEAD"))
+    private void onReloadPermissions(ServerPlayer player, CallbackInfo ci) {
         MultiCamCommon.sendUsagePacket(player);
     }
 }
