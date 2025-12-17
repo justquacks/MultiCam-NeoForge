d99c2430bcbd118c16502de92692b52f53543e10
justquacks <justquacksyt@gmail.com>
2025-12-17 04:24:12 -0600

    Update VanillaWindowContext.java

--- src/main/java/me/basiqueevangelist/windowapi/context/VanillaWindowContext.java
+++ src/main/java/me/basiqueevangelist/windowapi/context/VanillaWindowContext.java
@@ -1,81 +1,85 @@
 package me.basiqueevangelist.windowapi.context;
 
-import me.basiqueevangelist.windowapi.SupportsFeaturesImpl;
+import com.mojang.blaze3d.pipeline.RenderTarget;
+import com.mojang.blaze3d.platform.Window;
 import me.basiqueevangelist.windowapi.WindowFramebufferResized;
-import net.fabricmc.fabric.api.event.Event;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.Framebuffer;
-import net.minecraft.client.util.Window;
-import org.jetbrains.annotations.ApiStatus;
+import net.minecraft.client.Minecraft;
+import net.neoforged.neoforge.common.NeoForge;
 
 import java.util.WeakHashMap;
+import java.util.function.Consumer;
 
-public class VanillaWindowContext extends SupportsFeaturesImpl<WindowContext> implements WindowContext {
+public class VanillaWindowContext implements WindowContext {
     private static final WeakHashMap<Window, VanillaWindowContext> MAP = new WeakHashMap<>();
 
-    public static final VanillaWindowContext MAIN = new VanillaWindowContext(MinecraftClient.getInstance().getWindow(), MinecraftClient.getInstance().getFramebuffer());
+    public static final VanillaWindowContext MAIN =
+            new VanillaWindowContext(
+                    Minecraft.getInstance().getWindow(),
+                    Minecraft.getInstance().getMainRenderTarget()
+            );
 
     private final Window window;
-    private final Framebuffer framebuffer;
+    private final RenderTarget framebuffer;
 
-    private final Event<WindowFramebufferResized> framebufferResizedEvents = WindowFramebufferResized.newEvent();
-
-    private VanillaWindowContext(Window window, Framebuffer framebuffer) {
+    private VanillaWindowContext(Window window, RenderTarget framebuffer) {
         this.window = window;
         this.framebuffer = framebuffer;
-
         MAP.put(window, this);
     }
 
-    @ApiStatus.Internal
     public static void onWindowResized(Window window) {
         var ctx = MAP.get(window);
         if (ctx == null) return;
-        ctx.framebufferResizedEvents.invoker().onFramebufferResized(window.getFramebufferWidth(), window.getFramebufferHeight());
+        NeoForge.EVENT_BUS.post(
+                new WindowFramebufferResized(
+                        window.getWidth(),
+                        window.getHeight()
+                )
+        );
     }
 
     @Override
     public int framebufferWidth() {
-        return window.getFramebufferWidth();
+        return window.getWidth();
     }
 
     @Override
     public int framebufferHeight() {
-        return window.getFramebufferHeight();
+        return window.getHeight();
     }
 
     @Override
-    public Event<WindowFramebufferResized> framebufferResized() {
-        return framebufferResizedEvents;
-    }
-
-    @Override
-    public Framebuffer framebuffer() {
+    public RenderTarget framebuffer() {
         return framebuffer;
     }
 
     @Override
     public int scaledWidth() {
-        return window.getScaledWidth();
+        return window.getGuiScaledWidth();
     }
 
     @Override
     public int scaledHeight() {
-        return window.getScaledHeight();
+        return window.getGuiScaledHeight();
     }
 
     @Override
     public double scaleFactor() {
-        return window.getScaleFactor();
+        return window.getGuiScale();
     }
 
     @Override
     public long handle() {
-        return window.getHandle();
+        return window.getWindow();
     }
 
     @Override
-    public String toString() {
-        return "VanillaWindowContext[" + (this == MAIN ? "MAIN" : window) + "]";
+    public void onFramebufferResized(Consumer<WindowContext> consumer) {
+        consumer.accept(this);
+    }
+
+    @Override
+    public <T> T get(Key<WindowContext, T> key) {
+        return null;
     }
 }
